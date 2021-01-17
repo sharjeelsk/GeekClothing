459edfbf719c56fb0f91a599b7213d3590d7f490
sharjeel shaikh <sharjeelarts@gmail.com>
2021-01-17 14:49:37 +0530

    updated menutimes to navigate with router hoc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
 import {HomePage} from './pages/homepage/homepage.component'
+import {Switch, Route} from 'react-router-dom'
+
+
+const HatsPage=()=>{
+  return(
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+     <Route exact={true} path="/" component={HomePage} />
+     <Route path="/hats" component={HatsPage} />
+     </Switch>
     </div>
   );
 }
